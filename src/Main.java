25ddbbdfb25cb7178c0d92d619af5673286956bd
Petrov <ptv10378@gmail.com>
2022-02-19 20:15:52 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 1520; // цена билета
+        int rubles = 20; // количество рублей за одну бонусную миллю
+
+        //логика
+        System.out.println((price / rubles) + "Количество начисленных миль");
+    }
+}
